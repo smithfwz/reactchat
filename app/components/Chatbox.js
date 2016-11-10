c3f1eeb3da174dee89b493511030008d7758eab4
Smith <smith@futureworkz.com>
2016-11-10 22:54:46 +0700

    Add Chatbox.js

---
+++ app/components/Chatbox.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import AddMessageForm from './AddMessageForm'
+import manageMessages from './ManageMessages'
+import Message from './Message'
+
+@observer class Chatbox extends React.Component {
+  componentWillMount() {
+    manageMessages.loadMessageDefault()
+  }
+
+  render() {
+    return (
+      <div className="chatbox">
+        <ul className="list-of-messages">
+          { 
+            manageMessages.messages &&
+            Object
+              .keys(manageMessages.messages)
+              .map( key => <Message 
+                            key={ key } 
+                            index={ key }
+                            details={ manageMessages.messages[key] }
+                            userId={ manageMessages.messages[key].sender }
+                            />
+                  )
+          }
+        </ul>
+
+        <AddMessageForm userId={ this.props.userId } />
+      </div>
+    )
+  }
+}
+
+export default Chatbox
